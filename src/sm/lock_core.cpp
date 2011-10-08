40030f141e6d09359c181a903250c20e6c5b5cce
Pinar Tozun <pinar.tozun@epfl.ch>
2011-10-08 19:17:12 +0200

    hideaki-patch-no2: solved the deadlock problem for tpce

--- src/sm/lock_core.cpp
+++ src/sm/lock_core.cpp
@@ -2676,12 +2676,15 @@ lock_core_m::_check_deadlock(xct_t* self,
 
         // This iterator is ok because we have the lock's head_mutex
         // and if we block, we'll restart the search anyway.
-        lock_head_t::safe_queue_iterator_t it(*lock); 
+        lock_head_t::safe_queue_iterator_t it(*lock);
+	bool so_far_everyone_granted = true;
+	bool ahead_of_us = true;
         for(int i=0; i < Qlen; i++)
         {
             req = it.next();
             if(req == myreq) 
             {
+		ahead_of_us = false;
                 // If we are not converting, then the only request we
                 // depend on should be ahead of us in the queue.
                 if(!converting) {
@@ -2701,8 +2704,36 @@ lock_core_m::_check_deadlock(xct_t* self,
                 continue;
             }
 
-            if(compat[req->mode()][mymode] ) {
-                if(DEBUG_DEADLOCK) 
+	    // compatibility is not just with predecessor's current lock mode.
+	    // Suppose this case.
+	    // Lock head A: T1-S-granted, T2-S-granted-upgrading-to-X, T3-S-waiting
+	    // Lock head B: T3-X-granted, T1-X-waiting
+	    // because T2 has prior upgrade-request, T3 can't get S lock on A.
+	    // We have to detect this as an incompatible case too.
+	    // see acquire_lock() code. see ticket:105
+	    bool predecessor_compatible;
+	    if (ahead_of_us && (req->status() == lock_m::t_waiting || req->status() == lock_m::t_converting)) {
+		if (so_far_everyone_granted) {
+		    // this is the first waiter ahead of us, so we are waiting for him too
+		    // regardless of lock mode
+		    predecessor_compatible = false;
+		    so_far_everyone_granted = false;
+		} else {
+		    // otherwise (2nd, 3rd... waiter ahead us), just check lock mode compatibility.
+		    // alternatively, we can always consider them as incompatible and it might detect deadlock
+		    // earlier (before the first waiter or this thread get victimized), but could
+		    // cause false positives instead (consider this: S, S->X (1st waiter), S(2nd),S,..., S(me) ).
+		    if (req->status() == lock_m::t_converting) {
+			predecessor_compatible = compat[req->mode()][mymode] && compat[req->convert_mode()][mymode];
+		    } else {
+			predecessor_compatible = compat[req->mode()][mymode];
+		    }
+		}
+	    } else {
+		predecessor_compatible = compat[req->mode()][mymode];
+	    }
+	    if(predecessor_compatible) {
+		if(DEBUG_DEADLOCK) 
                     fprintf(stderr, 
 "%p found compat i %d len %d status %d mode %d req %p (my status %d mode %d)\n",
                     myreq, i, Qlen,
