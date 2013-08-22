6368a44402de19a4a262d2099dcd4ffe3af3b344
Ryan Johnson <ryan.johnson@cs.utoronto.ca>
2013-08-22 08:39:36 -0400

    Added a new thread-local region allocator (not yet in use) * * * Added a new thread-local bump allocator (not in use yet)

---
+++ src/fc/w_temp_alloc.cpp
@@ -0,0 +1,183 @@
+/* -*- mode:C++; c-basic-offset:4 -*-
+     Shore-MT -- Multi-threaded port of the SHORE storage manager
+   
+                          Copyright (c) 2013
+
+                    Department of Computer Science
+                        University of Toronto
+                                   
+                         All Rights Reserved.
+   
+   Permission to use, copy, modify and distribute this software and
+   its documentation is hereby granted, provided that both the
+   copyright notice and this permission notice appear in all copies of
+   the software, derivative works or modified versions, and any
+   portions thereof, and that both notices appear in supporting
+   documentation.
+   
+   This code is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. THE AUTHORS
+   DISCLAIM ANY LIABILITY OF ANY KIND FOR ANY DAMAGES WHATSOEVER
+   RESULTING FROM THE USE OF THIS SOFTWARE.
+*/
+#include "w_temp_alloc.h"
+
+#include <pthread.h>
+#include <cstdio>
+#include <cassert>
+#include <cstdlib>
+#include <cstring>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <memory>
+
+#if !HAVE_DECL_MAP_ANONYMOUS
+#if HAVE_DECL_MAP_ANON
+#define MAP_ANONYMOUS MAP_ANON
+#else
+#error I need a way to create anonymous private mappings with mmap
+#endif
+#endif
+
+#ifndef SQL_POOL_MMAP_LIMIT
+#define SQL_POOL_MMAP_LIMIT (10*1024*1024)
+#endif
+
+#define SQL_POOL_DEBUG
+
+#ifdef SQL_POOL_DEBUG
+#define SPLOG(fmt, ...) fprintf(stderr, fmt, ##__VA_ARGS__)
+#else
+#define SPLOG(fmt, ...)
+#endif
+
+struct w_temp_pool {
+    char *buf;
+    size_t bufsz;
+    size_t end;
+    size_t hiwater;
+
+    void init();
+    void fini();
+    w_temp_alloc::ptr alloc(size_t sz);
+    void grow_buf();
+};
+
+static __thread w_temp_pool tpool;
+static pthread_key_t pool_key;
+static pthread_once_t pool_init_once = PTHREAD_ONCE_INIT;
+    
+static size_t system_pagesz;
+static size_t pool_buf_bound;
+
+
+w_temp_alloc::ptr w_temp_alloc::alloc(size_t sz) { return tpool.alloc(sz); }
+
+w_temp_alloc::mark::mark()
+    : end(tpool.end)
+{
+}
+
+w_temp_alloc::mark::~mark() {
+    if (tpool.end > tpool.hiwater)
+        tpool.hiwater = tpool.end;
+    
+    tpool.end = end;
+}
+
+extern "C" {
+    // pthread_key destructor
+    static void w_temp_alloc_pthread_exit(void* arg __attribute__((unused)) ) {
+        assert (arg == &tpool);
+        tpool.fini();
+    }
+
+    // atexit callback
+    static void w_temp_alloc_atexit() {
+        tpool.fini();
+    }
+            
+    static void w_temp_alloc_init() {
+        long rval = sysconf(_SC_PAGESIZE);
+        if (rval < 0) {
+            system_pagesz = 4096;
+            fprintf(stderr,
+                    "WARNING: Unable to determine the page size on this system.\n"
+                    "-> Defaulting to %zd\n", system_pagesz);
+        }
+        system_pagesz = rval;
+        pool_buf_bound = alignon(SQL_POOL_MMAP_LIMIT, system_pagesz);
+        
+        if (pthread_key_create(&pool_key, &w_temp_alloc_pthread_exit)) {
+            fprintf(stderr,
+                    "WARNING: Unable to register destructors for thread-local pool allocator\n"
+                    "-> thread-local pools will not be reclaimed until process exit!\n");
+        }
+        atexit(&w_temp_alloc_atexit);
+    }
+}
+
+void w_temp_pool::init() {
+    // man page says failure is always due to programmer error
+    if (pthread_once(&pool_init_once, &w_temp_alloc_init))
+        throw std::bad_alloc();
+
+    // register it so the thread destructor is called
+    pthread_setspecific(pool_key, this);
+    if (pthread_getspecific(pool_key) != this)
+        fprintf(stderr,
+                "WARNING: unable to register destructor for w_temp_pool of tid=%zx!\n"
+                "-> thread-local memory will not be reclaimed until process exit\n",
+                (uintptr_t) pthread_self());
+        
+    int flags = MAP_PRIVATE|MAP_ANONYMOUS;
+    buf = (char*) mmap(0, pool_buf_bound, PROT_NONE, flags, -1, 0);
+    if (buf == MAP_FAILED)
+        throw std::bad_alloc();
+
+}
+
+void w_temp_pool::fini() {
+    if (not buf)
+        return; // never used in this thread
+        
+    if (end > hiwater)
+        hiwater = end;
+        
+    SPLOG("Thread %zd w_temp_alloc peaked at %zd bytes\n",
+          (size_t)pthread_self(), hiwater);
+        
+    munmap(buf, pool_buf_bound);
+    bufsz = end = hiwater = 0;
+    buf = 0;
+}
+
+void w_temp_pool::grow_buf() {
+    size_t newsz = 3*bufsz/2;
+    if (not buf) {
+        init();
+        newsz = 1024;
+    }
+    if (bufsz == pool_buf_bound)
+        throw std::bad_alloc();
+    
+    newsz = alignon(newsz, system_pagesz);
+    if (newsz > pool_buf_bound)
+        newsz = pool_buf_bound;
+
+    if (mprotect(buf+bufsz, newsz-bufsz, PROT_READ|PROT_WRITE))
+        throw std::bad_alloc();
+    
+    bufsz = newsz;
+}
+
+w_temp_alloc::ptr w_temp_pool::alloc(size_t sz) {
+    size_t nbytes = align(sz);
+    if (bufsz - end < nbytes)
+        grow_buf();
+            
+    uint32_t rval = end;
+    end += nbytes;
+    return (w_temp_alloc::ptr) { sz, buf + rval };
+}
---
+++ src/fc/w_temp_alloc.h
@@ -0,0 +1,234 @@
+/* -*- mode:C++; c-basic-offset:4 -*-
+     Shore-MT -- Multi-threaded port of the SHORE storage manager
+   
+                          Copyright (c) 2013
+
+                    Department of Computer Science
+                        University of Toronto
+                                   
+                         All Rights Reserved.
+   
+   Permission to use, copy, modify and distribute this software and
+   its documentation is hereby granted, provided that both the
+   copyright notice and this permission notice appear in all copies of
+   the software, derivative works or modified versions, and any
+   portions thereof, and that both notices appear in supporting
+   documentation.
+   
+   This code is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. THE AUTHORS
+   DISCLAIM ANY LIABILITY OF ANY KIND FOR ANY DAMAGES WHATSOEVER
+   RESULTING FROM THE USE OF THIS SOFTWARE.
+*/
+#ifndef __W_TEMP_ALLOC_H
+#define __W_TEMP_ALLOC_H
+
+#include "w_defines.h"
+
+#include <utility>
+#include <cstring>
+
+/* A thread-local region allocator, designed for objects with a
+   lifetime similar to that of a variable on the stack, but which are
+   inconvenient to actually allocate on the stack for some
+   reason. Examples include objects whose size is unknown at compile
+   time, or whose allocation is performed by a helper function.
+
+   The allocator is *extremely* fast due to its extreme simplicity: a
+   "bump" allocator that remembers only how much memory it has
+   dispensed so far. Creating a w_temp_alloc::mark object records the
+   allocator's current state, which will be restored when the mark
+   goes out of scope, freeing all memory that was allocated by that
+   thread during its lifetime. As with calling malloc() or placement
+   new, the user must call destructors manually if such is required.
+
+   As with stack-allocated storage, it is unsafe to assume that memory
+   obtained from w_temp_alloc can be passed to other threads or stored
+   after the current function returns, because a w_temp_alloc::mark
+   could be about to go out of scope in the caller. However, a
+   function *can* safely return memory obtained from w_temp_alloc to
+   the caller, because the caller knows when (or whether) any of its
+   marks will invalidate the memory.
+
+   There is also an STL-compatible allocator based on this allocator,
+   for efficient allocation of temporary containers, strings, etc.
+*/
+struct w_temp_alloc {
+    struct mark {
+        size_t end;
+        mark();
+        ~mark();
+    };
+    
+    /* A pointer to storage that may have been allocated by
+       w_temp_alloc and should not be passed to ::free or operator
+       delete. Unlike w_temp_alloc::buf, it is POD (no constructor or
+       destructor) and does *not* allocate storage when initialized or
+       copied.
+    */
+    struct ptr {
+        size_t _bufsz;
+        char *_buf;
+
+        struct _decay {
+            char *buf;
+            template <typename T>
+            operator T*() const { return (T*) buf; }
+        };
+
+        struct _decay_const {
+            char const *buf;
+            template <typename T>
+            operator T const*() const { return (T const*) buf; }
+        };
+
+        bool is_valid() const { return _buf; }
+        size_t size() const { return _bufsz; }
+    
+        _decay decay() { return (_decay) { _buf }; }
+        _decay_const decay() const { return (_decay_const) { _buf }; }
+    
+        // change the (perceived) size of this buf
+        void resize(size_t newsz) {
+            if (newsz > _bufsz) 
+                *this = alloc(newsz);
+            else
+                _bufsz = newsz;
+        }
+
+        void clear() {
+            _bufsz = 0;
+            _buf = 0;
+        }
+    };
+
+    // Allocate a chunk of memory
+    static ptr alloc(size_t sz);
+
+    /* Convenience method: an instance of w_temp_alloc decays to a
+       pointer of any type, as well as w_temp_alloc::ptr. This allows
+       the following idiom, for example:
+
+       w_temp_alloc::ptr p1 = w_temp_alloc(32);
+       void *p2 = w_temp_alloc(48);
+       char *p3 = w_temp_alloc(100);
+       
+     */
+    explicit w_temp_alloc(size_t sz)
+        : _ptr(alloc(sz))
+    {
+    }
+
+    operator ptr() { return _ptr; }
+
+    template <typename T>
+    operator T*() { return _ptr.decay(); }
+
+    /* An object that represents the value of a chunk of memory. When
+       copied, it allocates new storage and copies the contents over
+       (unlike w_temp_alloc::ptr, which merly copies the pointer).
+     */
+    struct buf : ptr {
+        explicit buf(size_t sz=0)
+            : ptr(alloc(sz))
+        {
+        }
+
+        /* wrap a pointer to an arbitrary chunk of memory (not
+           necessarily allocated by w_temp_alloc)
+        */
+        buf(size_t sz, char *ptr) {
+            _bufsz = sz;
+            _buf = ptr;
+        }
+
+        // copy a chunk of memory
+        buf(buf const &v)
+            : ptr(alloc(v._bufsz))
+        {
+            memcpy(_buf, v._buf, v._bufsz);
+        }
+
+        // copy an existing buffer into this one
+        buf &operator=(buf v);
+
+        ~buf() { clear(); }
+    };
+
+    // an STL-compatible allocator
+    template <typename T>
+    struct allocator {
+        typedef T value_type;
+
+        typedef T *pointer;
+        typedef T const *const_pointer;
+        
+        typedef T &reference;
+        typedef T const &const_reference;
+
+        typedef size_t size_type;
+        typedef ptrdiff_t difference_type;
+
+        allocator() { }
+        allocator(allocator const &) { }
+        template <typename U>
+        allocator(allocator<U> const &) { }
+
+        T* allocate(size_t n, void* =0) {
+            return (T*) alloc(n*sizeof(value_type)).decay();
+        }
+        
+        void deallocate(T*, size_t) { /* nop */ }
+        
+        size_type max_size() const { return size_t(-8); }
+        
+        template <class U> struct rebind { typedef allocator<U> other; };
+
+#ifdef USE_CXX11
+        template <typename... Args>
+        void construct(pointer p, Args... args) {
+            new (p) value_type(std::forward<Args>(args)...);
+        }
+#endif
+
+        void construct(pointer p, const_reference v) {
+            new (p) value_type(v);
+        }
+
+        void destroy(pointer p) {
+            p->~value_type();
+        }
+    };
+
+    ptr _ptr;
+};
+
+// efficient swap for w_temp_alloc::buf (no copying involved) 
+inline
+void swap(w_temp_alloc::buf &a, w_temp_alloc::buf &b) {
+    using std::swap;
+    swap(a._bufsz, b._bufsz);
+    swap(a._buf, b._buf);
+}
+
+inline
+w_temp_alloc::buf &w_temp_alloc::buf::operator=(w_temp_alloc::buf v) {
+    using std::swap;
+    swap(*this, v);
+    return *this;
+}
+
+
+// stupid, but required by the STL...
+template <typename A, typename B>
+bool operator==(w_temp_alloc::allocator<A> const &, w_temp_alloc::allocator<B> const &) {
+    return true;
+}
+
+template <typename A, typename B>
+bool operator!=(w_temp_alloc::allocator<A> const &, w_temp_alloc::allocator<B> const &) {
+    return false;
+}
+
+#endif
